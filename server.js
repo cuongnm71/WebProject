ba0d12b9f40c64ec2d8e5fe9adc14ac158f7aa26
Cuong <nmc7198@gmail.com>
2019-05-02 00:01:31 +0700

    template

---
+++ server.js
@@ -0,0 +1,39 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url); // connect tới database
+
+// require('./config/passport')(passport); //
+
+// Cấu hình ứng dụng express
+app.use(morgan('dev')); // sử dụng để log mọi request ra console
+app.use(cookieParser()); // sử dụng để đọc thông tin từ cookie
+app.use(bodyParser()); // lấy thông tin từ form HTML
+
+app.set('view engine', 'ejs'); // chỉ định view engine là ejs
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+app.get('/about', (req, res) => {
+  res.render('about.ejs')
+})
+// app.use(session({ secret: 'xxxxxxxxxxxxx' }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
+
+// require('./app/routes.js')(app, passport); // load các routes từ các nguồn
+
+app.listen(port);
