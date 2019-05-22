4d7dd48e4f5e4fdb005e5841870289412c5b7813
hoangle98 <hoangle12298@gmail.com>
2019-05-22 14:59:53 +0700

    update unit_management layout

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
-// module.exports = {
-//     'connection': {
-//         host: 'k61iotlab.duckdns.org',
-//         user: 'root',
-//         password: 'iotlab2018'
-//     },
-//     database: 'uFaculties',
-//     'user_table': 'user_account'
-// }
-
 module.exports = {
     'connection': {
-        host: 'localhost',
+        host: 'k61iotlab.duckdns.org',
         user: 'root',
-        password: '12345678'
+        password: 'iotlab2018'
     },
-    database: 'ufaculties',
+    database: 'uFaculties',
     'user_table': 'user_account'
 }
+
+// module.exports = {
+//     'connection': {
+//         host: 'localhost',
+//         user: 'root',
+//         password: '12345678'
+//     },
+//     database: 'ufaculties',
+//     'user_table': 'user_account'
+// }
