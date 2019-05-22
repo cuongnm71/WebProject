8d515f4296f96d1099eafed63c0b7fcc5e5f80c3
Cuong <nmc7198@gmail.com>
2019-05-22 13:40:32 +0700

    merge 2

--- app.js
+++ app.js
@@ -15,10 +15,84 @@ require('./config/passport')(passport);
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
- extended: true
+    extended: true
 }));
 
-app.use(express.static(__dirname + '/public'));
+app.use("/public",express.static(__dirname + '/public'));
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// contact page
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+});
+
+app.get('/unit_a',function(req,res){
+    res.render('pages/unit_management');
+});
+app.get('/off_a',function(req,res){
+    res.render('pages/officier_management');
+});
+app.get('/res_a',function(req,res){
+    res.render('pages/research_area_management');
+});
+
+var random_id = function  ()
+{
+    var id_num = Math.random().toString(9).substr(2,3);
+    var id_str = Math.random().toString(36).substr(2);
+    return id_num + id_str;
+}
+var fakeDatabase = {
+    'dv1':{
+        name:'Bộ môn truyền thông mạng máy tính'
+        ,type:'Bộ môn'
+        ,add:'406-E3'
+        ,phone:''
+        ,site:''
+    },
+    'dv2':{
+        name:'Bộ môn khoa học máy tính'
+        ,type:'Bộ môn'
+        ,add:''
+        ,phone:''
+        ,site:''
+    }
+};
+app.get('/units',(req,res)=>{
+    var allDV = Object.keys(fakeDatabase);
+    console.log('running app.get /units get data: ', allDV);
+    //res.send(allDV);
+    res.send(fakeDatabase);
+});
+/*
+var counttt  = 9;
+app.post('/units',(req,res)=>{
+    console.log(req.body);
+    var comd = res.body.command;
+    if("insert_row".equal(comd))
+    {
+        var s = '' + ++counttt;
+        res.send({id:s})
+    }
+});*/
+
+app.get('/units/:unitid',(req,res)=>{
+    var nameToLookup = req.params.unitid;
+    console.log(nameToLookup);
+    var value = fakeDatabase[nameToLookup];
+    console.log(nameToLookup,'->',value);
+    if(value){
+        res.send(value);
+    } else {
+        res.send({});
+    }
+    /*delete fakeDatabase.dv1;
+    console.log(fakeDatabase);*/
+});
 app.set('views', path.join(__dirname, '/views'));
 
 app.set('view engine', 'ejs');
@@ -33,7 +107,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-require('./routes/index.js')(app, passport);
+// require('./routes/index.js')(app, passport);
 
 app.listen(port);
 console.log("Port: " + port);
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,106 @@
 module.exports = function(app, passport) {
+    // index page
     app.get('/', function(req, res) {
         res.render('pages/index');
     });
 
-    app.get('/login', function(req, res) {
-        res.render('login.ejs', {
-            message: req.flash('loginMessage')
-        });
+    // contact page
+    app.get('/contact', function(req, res) {
+        res.render('pages/contact');
     });
 
-    app.post('/login', passport.authenticate('local-login', {
-            failureRedirect: '/login',
-            failureFlash: true
-        }),
-        function(req, res) {
-            console.log(req.user.isAdmin);
-            // if (req.body.remember) {
-            //     req.session.cookie.maxAge = 1000 * 10;
-            // } else {
-            //     req.session.cookie.expires = false;
-            // }
-            if (req.user.isAdmin === 1) {
-              res.redirect('pages/admin');
-            }
-            if (req.user.isAdmin === 0) {
-              res.redirect('/profile');
-            }
-        });
-
-    app.get('/admin', function(req, res) {
-        res.render('pages/index.ejs');
+    app.get('/unit_a',function(req,res){
+        res.render('pages/unit_management');
     });
+    app.get('/off_a',function(req,res){
+        res.render('pages/officier_management');
+    });
+    app.get('/res_a',function(req,res){
+        res.render('pages/research_area_management');
+    });
+
+    var random_id = function  ()
+    {
+        var id_num = Math.random().toString(9).substr(2,3);
+        var id_str = Math.random().toString(36).substr(2);
+        return id_num + id_str;
+    }
+    var fakeDatabase = {
+        'dv1':{
+            name:'Bộ môn truyền thông mạng máy tính'
+            ,type:'Bộ môn'
+            ,add:'406-E3'
+            ,phone:''
+            ,site:''
+        },
+        'dv2':{
+            name:'Bộ môn khoa học máy tính'
+            ,type:'Bộ môn'
+            ,add:''
+            ,phone:''
+            ,site:''
+        }
+    };
+    app.get('/units',(req,res)=>{
+        var allDV = Object.keys(fakeDatabase);
+        console.log('running app.get /units get data: ', allDV);
+        //res.send(allDV);
+        res.send(fakeDatabase);
+    });
+    /*
+    var counttt  = 9;
+    app.post('/units',(req,res)=>{
+        console.log(req.body);
+        var comd = res.body.command;
+        if("insert_row".equal(comd))
+        {
+            var s = '' + ++counttt;
+            res.send({id:s})
+        }
+    });*/
+
+    app.get('/units/:unitid',(req,res)=>{
+        var nameToLookup = req.params.unitid;
+        console.log(nameToLookup);
+        var value = fakeDatabase[nameToLookup];
+        console.log(nameToLookup,'->',value);
+        if(value){
+            res.send(value);
+        } else {
+            res.send({});
+        }
+        /*delete fakeDatabase.dv1;
+        console.log(fakeDatabase);*/
+    });
+
+    // app.get('/login', function(req, res) {
+    //     res.render('login.ejs', {
+    //         message: req.flash('loginMessage')
+    //     });
+    // });
+    //
+    // app.post('/login', passport.authenticate('local-login', {
+    //         failureRedirect: '/login',
+    //         failureFlash: true
+    //     }),
+    //     function(req, res) {
+    //         console.log(req.user.isAdmin);
+    //         // if (req.body.remember) {
+    //         //     req.session.cookie.maxAge = 1000 * 10;
+    //         // } else {
+    //         //     req.session.cookie.expires = false;
+    //         // }
+    //         if (req.user.isAdmin === 1) {
+    //           res.redirect('pages/admin');
+    //         }
+    //         if (req.user.isAdmin === 0) {
+    //           res.redirect('/profile');
+    //         }
+    //     });
+    //
+    // app.get('/admin', function(req, res) {
+    //     res.render('pages/index.ejs');
+    // });
 
 //     app.get('/signup', function(req, res) {
 //         res.render('signup.ejs', {
